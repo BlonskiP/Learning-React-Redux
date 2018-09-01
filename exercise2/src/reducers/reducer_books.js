1c7638945f4220589ba2bc64f87720f6dd2bf8e5
blonski <piotrblonski96@gmail.com>
2018-09-02 01:10:29 +0200

    Exercise2

---
+++ exercise2/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return  [
+      { title:'Javascript: The Good Parts', pages:101},
+      { title:'harry Potter', pages:221 },
+      { title:'The Dark Tower', pages:32 },
+      { title:'Eloquenty Ruby', pages:1 } 
+    ];
+}
