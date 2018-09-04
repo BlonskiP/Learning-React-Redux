598bcdbffc36356149f0225e96f77f4bbf8b7f96
Yrsa <falka164@gmail.com>
2018-09-04 03:07:46 +0200

    Revert "Merge branch 'master' into Styles"
    This reverts commit b66a95622ee8378317b7058a5d6e3fbc2199e7bf.

--- API_Learing/src/index.js
+++ API_Learing/src/index.js
@@ -1,47 +1,15 @@
-import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-//tworzenie componentu w reacie
-const API_KEY = 'AIzaSyDAPeaGHGm0uRbLxkE6_mdf1bcDLhWz2Bk';
+import App from './components/app';
+import reducers from './reducers';
 
-class App extends React.Component
-{
-    constructor(props){
-        super(props);
-        this.state ={
-            videos: [],
-            selectedVideo:null
-        };
-        this.videoSearch('Pokemons')
-    }
-    videoSearch(term)
-    {
-        YTSearch
-            ({ key: API_KEY, term: term },
-            (videos) => {
-                this.setState({
-                    videos: videos,
-                    selectedVideo: videos[0]
-                });
-            }
-            );
-    }
-    render()
-    {
-        const videoSearch =_.debounce((term)=>{this.videoSearch(term)},300);
-        return <div>
-            <SearchBar onSearchTermChange={videoSearch}/>
-            <VideoDetail video={this.state.selectedVideo} />
-            <VideoList 
-            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-            videos={this.state.videos} />
-          </div>;
-    }
-}
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
