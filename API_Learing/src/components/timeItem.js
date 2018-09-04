a81fff17c34897eb639b23b0dfa6202535a17867
blonski <piotrblonski96@gmail.com>
2018-09-04 02:52:45 +0200

    TimeSaveList

---
+++ API_Learing/src/components/timeItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DelBtn from './DelBtn';
+class SaveListItem extends React.Component
+{
+render(){
+    return(
+    <span>
+    <li>21:37:50</li><DelBtn />
+    </span>
+    )
+}
+}
+export default SaveListItem;
---
+++ API_Learing/src/containers/saveTimeList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TimeItem from '../components/timeItem';
+class SaveTimeList extends React.Component
+{
+    render()
+    {return(
+        <div>
+        <TimeItem/>
+        <TimeItem/>
+        <TimeItem/>
+        <TimeItem/>
+        <TimeItem/>
+        </div>
+     ) }
+}
+export default SaveTimeList;
