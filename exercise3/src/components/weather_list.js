2bde69b465981bfde8b8217a4f940d8ef316fe32
blonski <piotrblonski96@gmail.com>
2018-09-07 02:42:53 +0200

    End of Exercise3
    It works :D

--- exercise3/src/components/weather_list.js
+++ exercise3/src/components/weather_list.js
@@ -1,35 +1,39 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
-
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map'
 class WeatherList extends Component{
 
     renderWeather(cityData){
 
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather =>weather.main.temp);
+        const temps = _.map(cityData.list.map(weather =>weather.main.temp), (temp)=>temp-273);
+        const pressures =  cityData.list.map(weather =>weather.main.pressure);
+        const humidities =  cityData.list.map(weather =>weather.main.humidity);
+       // const lon = cityData.city.coord.lon;
+       // const lat = cityData.city.coord.lat;
+       const {lon, lat} = cityData.city.coord;
         console.log(temps);
         return (
             <tr key={name}>
-                <td>{name}</td>
-                <td><Sparklines height={120} width={180} data={temps}>
-                    <SparklinesLine color="red"/>
-                </Sparklines>
-                    </td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
+                <td><Chart data={temps} color="orange" units="C"/></td>
+                <td><Chart data={pressures} color="green" units="hPa"/></td>
+                <td><Chart data={humidities} color="black" units="%"/></td>
             </tr>
-
         );
-
     }
+
+
     render(){
         return(
             <table className="table table-hover">
             <thead>
             <tr>
                 <th>City</th>
-                <th>Temperature</th>
-                <th>Presure</th>
-                <th>Humidity</th>
+                <th>Temperature (K)</th>
+                <th>Presure(hPa)</th>
+                <th>Humidity(%)</th>
             </tr>
             </thead>
             <tbody>
